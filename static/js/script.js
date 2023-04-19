41e42b0ec4f17aa66dcfbc1957211f6384197b7f
fum1h1to <75571344+fum1h1to@users.noreply.github.com>
2023-04-20 00:34:21 +0900

    クライアント側の枠組み作成

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,32 @@
-alert('err');
+// このアプリで扱うページのIDを配列に格納
+const pages = [
+  'top',
+  'problem',
+  'answer',
+  'problemError',
+  'answerError'
+]
+
+// 現在フォーカスされているページのID
+let nowPageId = 'top';
+
+/**
+ * pageを切り替えるための関数
+ * @param {string} pageId 
+ * @returns 
+ */
+const pageChanger = (pageId) => {
+  const basePageContainer = document.querySelectorAll('.js-basePageContainer');
+
+  if (!pages.includes(pageId)) {
+    console.error(pageId + 'というページは存在しません。');
+    return;
+  }
+  nowPageId = pageId;
+  basePageContainer.forEach((ele) => {
+    ele.classList.remove('is-active');
+    if (ele.id === pageId) {
+      ele.classList.add('is-active');
+    }
+  });
+}
