39c42094e6c1f76901773f15a6479ee2efc73295
fum1h1to <75571344+fum1h1to@users.noreply.github.com>
2023-04-20 23:41:40 +0900

    ページ切り替え処理修正

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,22 @@
 // このアプリで扱うページのIDを配列に格納
 const pages = [
-  'top',
-  'problem',
-  'answer',
-  'problemError',
-  'answerError'
-]
+  {
+    id: 'top',
+    url: '/',
+  }, {
+    id: 'problem',
+    url: '/problem',
+  }, {
+    id: 'answer',
+    url: '/answer',
+  }, {
+    id: 'problemError',
+    url: '/problemError',
+  }, {
+    id: 'answerError',
+    url: '/answerError',
+  }
+];
 
 // 現在フォーカスされているページのID
 let nowPageId = 'top';
@@ -17,16 +28,34 @@ let nowPageId = 'top';
  */
 const pageChanger = (pageId) => {
   const basePageContainer = document.querySelectorAll('.js-basePageContainer');
+  let isFound = false;
 
-  if (!pages.includes(pageId)) {
-    console.error(pageId + 'というページは存在しません。');
-    return;
-  }
-  nowPageId = pageId;
-  basePageContainer.forEach((ele) => {
-    ele.classList.remove('is-active');
-    if (ele.id === pageId) {
-      ele.classList.add('is-active');
+  pages.forEach((page) => {
+    if (page.id === pageId) {
+
+      isFound = true;
+      nowPageId = pageId;
+      window.history.pushState(null, null, page.url);
+
+      basePageContainer.forEach((ele) => {
+
+        ele.classList.remove('is-active');
+        if (ele.id === pageId) {
+          ele.classList.add('is-active');
+        }
+      });
     }
   });
+
+  if (!isFound) {
+    console.error(pageId + 'というページは存在しません。');
+  }
 }
+
+window.addEventListener("popstate", () => {
+  pages.forEach((page) => {
+    if (page.url === window.location.pathname) {
+      pageChanger(page.id);
+    }
+  });
+});
